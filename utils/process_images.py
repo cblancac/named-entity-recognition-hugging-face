f29c053dcd2aabfcc3c759a6f1031667a96da487
cblancac <carblacac7@gmail.com>
2024-03-03 15:55:46 +0100

    reduce dpi per image

--- utils/process_images.py
+++ utils/process_images.py
@@ -13,7 +13,7 @@ class ProcessImage:
 
     def convert_pdf_to_images(self, file_path: str) -> List[PngImageFile]:
         """Take one pdf file and convert it to one image per page"""
-        return convert_from_path(file_path, dpi=300, fmt="png")
+        return convert_from_path(file_path, dpi=150, fmt="png")
 
     def get_size_per_image(self, images: List[PngImageFile]) -> List[Tuple[int]]:
         """Get the size of all the images in a document"""
